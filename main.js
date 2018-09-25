7b583b526d54a5784399964f37a3b01ac80d9aaf
Турукин Илья Вячеславович <turukiniv@sovcombank.ru>
2018-09-25 16:12:32 +0400

    first up

---
+++ main.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    $('button').prop('disabled', true);
+    $('.background_preloader').css('display', 'block');         
+    $('.preloader').css('display', 'block');  
+});
